9054c2209a84d33ae97c02f731ac9ca604c0b859
Bernardo Trindade <btrindade@eeshoo.com>
2014-02-10 18:39:47 -0700

    finished main behavior to working state

--- src/fav_classes/Students.java
+++ src/fav_classes/Students.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 public class Students {
 	private Map<String, String> studentInfo;
-	
+
 	public Students(){
 		studentInfo = new HashMap<String, String>();
 	}
-	
+
 	public void add(String student_name, String fav_class){
 		studentInfo.put(student_name, fav_class);
 	}
-	
+
 	public void printStudentInfo(){
 		Set<String> student_names = studentInfo.keySet();
 		for (String name : student_names){
@@ -20,19 +20,29 @@ public class Students {
 	}
 
 	public static void main(String[] args) {
+		String studentName = new String();
+		String fav_class = new String();
+		Students class_list = new Students();
+		Scanner scn = new Scanner(System.in);
+
 		while(true){
-			String studentName = new String();
-			String fav_class = new String();
-			Scanner scn = new Scanner(System.in);
-			
 			System.out.println("Please enter the Student's name or 'Q' to quit:");
-			studentName = scn.next();
-			
-			if(studentName.toUpperCase() == "Q") break;
-			
-			System.out.println("Please enter the student's favorite class?");
+			studentName = scn.nextLine();
+
+			if(studentName.toUpperCase().contentEquals("Q")){
+				System.out.println("Quitting...");
+				break;
+			}
+			else{
+
+				System.out.println("Please enter the student's favorite class:");
+				fav_class = scn.nextLine();
+
+				class_list.add(studentName, fav_class);
 			}
 		}
+
+		class_list.printStudentInfo();
 
 	}
 
